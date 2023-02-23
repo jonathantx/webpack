6222360656f16d0208c8a8673c1902c486c534f2
Jonathan Teixeira <jonathancateixeira@outlook.com>
2023-02-23 16:35:26 -0300

    commit

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,56 +1,49 @@
-const modoDev = process.env.NODE_ENV !== 'production'
-const webpack  = require('webpack')
-const miniCssExtractPlugin = require('mini-css-extract-plugin')
-const css_minimizer_webpack_plugin = require('css-minimizer-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-
+const modoDev = process.env.NODE_ENV !== "production";
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+ 
 module.exports = {
-    mode: modoDev ? 'development' : 'production',
-    entry: '/src/principal.js',
-    output: {
-        filename: 'principal.js',
-        path: __dirname + '/public'
-    },
-    devServer: {
-        contentBase: "./public",
-        port: 9000
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin(),
-            new css_minimizer_webpack_plugin()
-        ],
-    },
-    plugins: [
-        new miniCssExtractPlugin({
-            filename: "estilo.css"
-        }),
-        new TerserPlugin({
-            parallel: true,
-            terserOptions: {
-                ecma: 6,
-            },
-        }),
+  mode: modoDev ? "development" : "production",
+  entry: "./src/principal.js",
+  output: {
+    filename: "principal.js",
+    path: __dirname + "/public",
+  },
+  devServer: {
+    contentBase: "./public",
+    port: 9000,
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+      }),
+      new OptimizeCSSAssetsPlugin({}),
     ],
-    module: {
-        rules: [{   
-            test: /\.s?[ac]ss$/,
-            use: [
-                miniCssExtractPlugin.loader,
-                // 'style-loader', // Adiciona CSS a DOM injetando a TAG <style>
-                'css-loader', // interpreta @import / url()... 
-                'sass-loader'
-            ]
-        }, {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [ {
-                loader: 'file-loader',
-                options: {
-                  esModule: false
-                }
-              }]
-        }]
-    }
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "estilo.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+          "css-loader", // interpreta @import, url()...
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
